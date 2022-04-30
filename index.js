424df661d7af64aa62c9fe8da4ea20b88db68cbf
Rusha Shah <68645625+rusha27@users.noreply.github.com>
2022-04-30 19:33:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-console.log("server is on")
-function showdetail(){
-var num = document.getElementById("phone");
-var amount = document.getElementById("amount");
-var status=document.getElementById("status");
-// let note = document.getElementById("note");
-let date = new Date();
+console.log("server is on");
 
-alert(amount.value + " is sent to " +num.value + " successfully at "+ date );
-// status= status.innerHTML= amount.value + " is sent to " +num.value + " successfully at "+ date ;
+
+function returnValidatePhone() {
+  let x = document.forms["form"]["phone"].value;
+  if (x == "") {
+    alert("phone number required");
+    return false;
+  }
+  var p = /^\d{10}$/;
+  if (x.match(p)) {
+    return true;
+  } else {
+    alert("invalid contact number");
+    return false;
+  }
 }
+function returnvalidateAmount() {
+  var amount = document.forms["form"]["amount"].value;
+  if (isNaN(amount)) {
+    alert("please enter a valid amount");
+    return false;
+  }
+}
+
+function showdetail() {
+    var num = document.getElementById("phone");
+    var amount = document.getElementById("amount");
+    var status = document.getElementById("status");
+    // let note = document.getElementById("note");
+    let date = new Date();
+  
+    alert(amount.value + " is sent to " + num.value + " successfully at " + date);
+  }
